c4c020aec6a4f442c626faa1f0754063f10b629d
Ziad <zslah2800@gmail.com>
2025-12-25 07:29:45 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/example/edupro_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.edupro_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const EDUPROApp());
+}
+class EDUPROApp extends StatelessWidget {
+  const EDUPROApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+}
